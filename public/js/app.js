16c82224869cba7f8aad403b33955804305128b4
rfv234 <gromdusha154@gmail.com>
2023-06-02 13:29:03 +0700

    add news

--- public/js/app.js
+++ public/js/app.js
@@ -14988,12 +14988,20 @@ __webpack_require__.r(__webpack_exports__);
     return {
       news: [{
         photo: 'images/sw.jpg',
-        name: 'Что нового в Швейцарии?',
-        text: 'Всемирная туристская организация (UNWTO) опубликовала рейтинг самых посещаемых стран мира в 2022 году. России в этом списке нет, но из той же статистики видно, что по совокупным расходам на международные поездки граждане РФ обошли туристов из Швейцарии, Саудовской Аравии, Сингапура и многих других стран.'
+        name: 'Власти Швейцарии связали дефицит лекарств с конфликтом на Украине',
+        text: 'Перебои с поставками лекарств в Швейцарии выросли на 46% в период с 2021 по 2022 год, отчасти причиной этого является конфликт на Украине, говорится в опубликованном докладе минэкономики Швейцарии. В прошлом году бюро уведомлений зафиксировало 201 нарушение поставок, что стало новым рекордом. Это на 46% больше, чем в 2021 году (137 нарушений), и почти на 10% больше, чем в 2019 году (184). Более чем в половине случаев нарушения, наблюдавшиеся в 2022 году, были связаны с антибиотиками (35%) или обезболивающими (19%), - сказано в документе, опубликованном на сайте Федерального совета.'
       }, {
-        photo: null,
-        name: 'Тестовая новость',
-        text: 'Текст тестовой новости'
+        photo: 'images/2d2618c20d17f3e1921ec0d98f0e45.png',
+        name: 'Выбраться с МЖК: власти три года обещают построить новую дорогу на Доватора — спасет ли она от пробок',
+        text: 'Октябрьский район — второй по численности населения в Новосибирске. Из-за большого количества автомобилей, а зимой — еще и из-за гор снега, там часто собираются пробки, а в общественном транспорте сложно найти место. Но ситуацию хотят исправить, как минимум для жителей МЖК и Плющихинского жилмассива — власти уже несколько лет обещают начать строить новую дорогу на улице Доватора. Она должна пройти от улицы Татьяны Снежиной до Гусинобродского шоссе. НГС выяснил, где собираются проложить дорогу, во сколько она обойдется бюджету и когда автомобилисты смогут по ней ездить.'
+      }, {
+        photo: 'images/kakoe-morozhenoe-mozhno-est-esli-vy-na-diete_1656418757679199039.jpg',
+        name: 'Лето начинается с мороженого: где искать любимые эскимо, стаканчики и рожки',
+        text: 'Летом мороженого хочется практически каждый день: эскимо на прогулке, рожок после обеда и большой брикет для семейных вечеров. Не только вкусно, но и освежающе — для многих новосибирцев вкус лета складывается из любимого лакомства, свежих овощей и фруктов.'
+      }, {
+        photo: 'images/nissan-z-proto-lateral.jpg',
+        name: 'В Новосибирске начали предлагать новые японские спорткары за 9 миллионов — как они выглядят и сколько ждать машину',
+        text: 'Новосибирцам стали доступны для покупки японские спорткары — объявления публикуют новосибирские автодилеры, а везут авто через третьи страны несколько месяцев. Корреспондент НГС пообщался с продавцами и автоэкспертами и выяснил, откуда везут машины, во сколько они обойдутся и есть ли на такой сегмент спрос у сибиряков.'
       }]
     };
   }
@@ -15149,17 +15157,43 @@ __webpack_require__.r(__webpack_exports__);
 var _withScopeId = function _withScopeId(n) {
   return (0,vue__WEBPACK_IMPORTED_MODULE_0__.pushScopeId)("data-v-17134b44"), n = n(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.popScopeId)(), n;
 };
-var _hoisted_1 = {
+var _hoisted_1 = /*#__PURE__*/_withScopeId(function () {
+  return /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
+    id: "ngs"
+  }, "Народная премия NGS.RU", -1 /* HOISTED */);
+});
+var _hoisted_2 = {
+  id: "all_news"
+};
+var _hoisted_3 = /*#__PURE__*/_withScopeId(function () {
+  return /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h3", null, "Все новости", -1 /* HOISTED */);
+});
+var _hoisted_4 = /*#__PURE__*/_withScopeId(function () {
+  return /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("hr", null, null, -1 /* HOISTED */);
+});
+var _hoisted_5 = /*#__PURE__*/_withScopeId(function () {
+  return /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("hr", null, null, -1 /* HOISTED */);
+});
+var _hoisted_6 = {
   id: "news"
 };
-var _hoisted_2 = ["src"];
+var _hoisted_7 = /*#__PURE__*/_withScopeId(function () {
+  return /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("br", null, null, -1 /* HOISTED */);
+});
+var _hoisted_8 = ["src"];
+var _hoisted_9 = /*#__PURE__*/_withScopeId(function () {
+  return /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("br", null, null, -1 /* HOISTED */);
+});
+
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($data.news, function (item) {
-    return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_1, [item.photo ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("img", {
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, [_hoisted_1, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_2, [_hoisted_3, _hoisted_4, ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($data.news, function (novelty) {
+    return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("span", null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(novelty.name), 1 /* TEXT */), _hoisted_5]);
+  }), 256 /* UNKEYED_FRAGMENT */))]), ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($data.news, function (item) {
+    return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_6, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h2", null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(item.name), 1 /* TEXT */), _hoisted_7, item.photo ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("img", {
       key: 0,
       src: item.photo
-    }, null, 8 /* PROPS */, _hoisted_2)) : (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("v-if", true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h2", null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(item.name), 1 /* TEXT */), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("span", null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(item.text), 1 /* TEXT */)]);
-  }), 256 /* UNKEYED_FRAGMENT */);
+    }, null, 8 /* PROPS */, _hoisted_8)) : (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("v-if", true), _hoisted_9, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("span", null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(item.text), 1 /* TEXT */)]);
+  }), 256 /* UNKEYED_FRAGMENT */))], 64 /* STABLE_FRAGMENT */);
 }
 
 /***/ }),
@@ -15269,7 +15303,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n#news[data-v-17134b44] {\r\n    border: solid 2px;\r\n    margin-top: 15px;\r\n    padding: 10px;\n}\r\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n#ngs[data-v-17134b44] {\r\n  background-color: midnightblue;\r\n  width: 100%;\r\n  height: 50px;\r\n  color: greenyellow;\r\n  text-align: center;\r\n  font-size: 24px;\r\n  font-family: sans-serif;\n}\n#all_news[data-v-17134b44] {\r\n  width: 230px;\r\n  position: absolute;\r\n  left: 15%;\r\n  font-family: sans-serif;\n}\n#news[data-v-17134b44] {\r\n  border: solid 2px;\r\n  margin-top: 15px;\r\n  margin-left: 30%;\r\n  padding: 10px;\r\n  width: 700px;\r\n  font-size: 18px;\r\n  font-family: sans-serif;\n}\nimg[data-v-17134b44] {\r\n  width: 700px;\r\n  height: 400px;\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
